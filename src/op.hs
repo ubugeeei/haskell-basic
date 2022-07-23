872188c83ba0bbee519949517df9ce20d3a327ad
Ubugeeei <ubuge1122@gamil.com>
2022-07-23 21:44:31 +0900

    $!

--- src/op.hs
+++ src/op.hs
@@ -1,10 +1,27 @@
 main:: IO ()
 main = do
   dol
+  dolim
 
+-- !
 dol:: IO ()
 dol = print $ sum $ map (*2) $ filter even [1..10]
 -- print $ sum $ map (*2) $ filter even [1..10]
 -- print $ sum $ map (*2) [2, 4, 6, 8, 10]
 -- print $ sum $ [4, 8, 12, 16, 20]
 -- print 60
+
+-- $!
+-- immediately evaluate
+dolim = print $ sum $ map (*2) $ filter even [1..10]
+-- print $ sum $ map (*2) $ filter even [1..10]
+-- print $ sum $ map (*2) [2, 4, 6, 8, 10]
+-- print $ sum $ [4, 8, 12, 16, 20]
+-- print 60
+
+
+-- `seq`
+-- <-
+-- =<<
+-- >>=
+-- >>
