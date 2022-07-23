e9baa12517e21e3703d03537f2151a22a5562868
Ubugeeei <ubuge1122@gamil.com>
2022-07-23 19:50:57 +0900

    types

---
+++ src/types.hs
@@ -0,0 +1,35 @@
+main:: IO ()
+main =putStrLn "Hello!"
+
+b::Bool
+b = True
+
+c::Char
+c = 'a'
+
+s::String
+s = "abcd"
+
+i::Int
+i = 123
+
+_i::Integer
+_i = 123
+
+f::Float
+f = 123.456
+
+d::Double
+d = 123.456
+
+is::[Int]
+is = [1,2,3]
+
+cs::[Char]
+cs = ['a','b','c']
+
+tp::(Int, Char)
+tp = (1,'a')
+
+i2i::Int -> Int
+i2i n = n + 1
