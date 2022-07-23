31e02cc64e3b7fe8476aedde6c62268591d4fb59
Ubugeeei <ubuge1122@gamil.com>
2022-07-23 19:33:40 +0900

    do

---
+++ src/do.hs
@@ -0,0 +1,8 @@
+main:: IO ()
+main = do { putStrLn "Red"; putStrLn "Green"; putStrLn "Blue"; layout }
+
+layout:: IO ()
+layout = do
+  putStrLn "Red"
+  putStrLn "Green"
+  putStrLn "Blue"
--- src/do_block.hs
+++
@@ -1,2 +0,0 @@
-main:: IO ()
-main = do { putStrLn "Red"; putStrLn "Green"; putStrLn "Blue" }
