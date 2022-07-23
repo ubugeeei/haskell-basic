219c16c449ad0633fd567d2a5e037e2162a9a9a8
Ubugeeei <ubuge1122@gamil.com>
2022-07-23 20:10:24 +0900

    list defs

--- src/l_def.hs
+++ src/l_def.hs
@@ -2,9 +2,21 @@ main:: IO ()
 main = do
   print list
   print list2
+  print list3
+  print list4
+  print list5
 
 list :: [Int]
 list = [1, 2, 3]
 
 list2 :: [Int]
 list2 = [1..3]
+
+list3 :: [Int]
+list3 = 1:[2, 3]
+
+list4 :: [Int]
+list4 = 1:2:[3]
+
+list5 :: [Int]
+list5 = 1:2:3:[]
